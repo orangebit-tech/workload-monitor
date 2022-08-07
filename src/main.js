9c7d36a12c0b866f6482727999ae1a164b9b08b0
orangebit-tech <orangebitt@gmail.com>
2022-08-07 13:52:13 +0000

    icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import VueRouter            from 'vue-router'
 import { router }           from './router/router'
 import store                from './store'
 import dotenv from 'dotenv'
+import ViewCompact          from 'vue-material-design-icons/ViewCompact'
+import ClipboardCheck       from 'vue-material-design-icons/ClipboardCheck'
+import Cog                  from 'vue-material-design-icons/Cog'
+import ChevronDown          from 'vue-material-design-icons/ChevronDown'
+
+
 
 dotenv.config()
 
@@ -11,7 +17,10 @@ Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 Vue.config.devtools = false;
-
+Vue.use(ViewCompact)
+Vue.use(ClipboardCheck)
+Vue.use(Cog)
+Vue.use(ChevronDown)
 new Vue({
   store,
   router,
