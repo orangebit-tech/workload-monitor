28cc3aea6fbc4b69780e2e1a6fc9b9d18493dc8f
orangebit-tech <orangebitt@gmail.com>
2022-08-30 07:06:30 +0000

    cleanup, count function added + search by key

--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,6 @@
 export default {
     sortedIssues(items, filters, orderBy){
-        console.log("SORT STARTED, items: ", items, ' order by: ', orderBy)
-        var customfields_10106 = []
-        console.log(customfields_10106)
         var localitems = [...items]
-        console.log('localitems: ', localitems)
-        // store.getters.getPmTasks.map((it) => {
-        //     if(!it.fields.customfield_10106){
-        //        customfields_10106.push(it) 
-        //     }
-        // })
-        console.log("FILTERS! ", filters)
         if(filters && filters.length > 0 && filters.includes('priority')){
             localitems = localitems.sort((a, b) => {
                 return parseInt(a.fields.priority.id) - parseInt(b.fields.priority.id)
@@ -21,7 +11,6 @@ export default {
                 return parseInt(a.id) - parseInt(b.id)
             })
         }
-        console.log('missing customfields_10106 in pmTasks', customfields_10106)
         var result = []
         var teams = []
         var pms = []
@@ -46,11 +35,9 @@ export default {
             'qa research'
         ]
         var all = []
-        
         if(orderBy == 'assignee'){
             var assigneesBlackList = []
             if(localitems){
-                console.log("ASSIGNEES SORT STARTED", localitems)
                 localitems.map(item => {
                     var assignee = item.fields.assignee?.displayName ? item.fields.assignee.displayName : "Unassigned"
                     var status = item.fields.status.name.toLowerCase()
@@ -83,7 +70,6 @@ export default {
         if(orderBy == 'developer' ){
             var developersBlackList = []
             if(localitems){
-                console.log("DEVS SORT STARTED", localitems)
                 localitems.map(item => {
                     var developer = item.fields.customfield_10107?.displayName ? item.fields.customfield_10107.displayName : "Unassigned"
                     var status = item.fields.status.name.toLowerCase()
@@ -117,7 +103,6 @@ export default {
         if(orderBy == 'team' || !orderBy){
             var teamsBlackList = []
             if(localitems){
-                console.log("TEAMS SORT STARTED", localitems)
                 localitems.map(item => {
                     var status = item.fields.status.name.toLowerCase()
                     var team = item.fields.customfield_10102?.value ? item.fields.customfield_10102.value : "Unassigned"
@@ -150,10 +135,7 @@ export default {
         if(orderBy == 'pm'){
             var pmsBlackList = []
             if(localitems){
-                console.log("PM SORT STARTED", localitems)
-                console.log("LOCAL ITEMS", localitems)
                 localitems.map(item => {
-                    // var status = item.fields.status.name.toLowerCase()
                     var status = ''
                     function itemStatus (status) {
                         item.fields.status.name.toLowerCase()
@@ -175,8 +157,6 @@ export default {
                     pms.push(pm)
 
                     if(!pmsBlackList.includes(pm)){
-
-                        
                         if(item.fields.customfield_10106 && !customers.includes(item.fields.customfield_10106.displayName)){
                             customers.push(item.fields.customfield_10106.displayName)
                         }
@@ -187,7 +167,6 @@ export default {
                         if(!result[pm][status]){
                             result[pm][status] = []
                         }
-
                         // sort filter statuses
                         if( item.fields.status &&  
                             (desiredStatusPM.includes(item.fields.status.name.toLowerCase()) 
@@ -205,7 +184,6 @@ export default {
                 assigneesBlackList = [...filters.hiddenAssignees]
             }
         }
-        console.log("FINAL RESULT", result)
         return result
     },
     getPriority(priority_id){
@@ -214,7 +192,7 @@ export default {
                 id: 1,
                 priority: 'Emengency',
                 priority_id: '7',
-                color: '#d53e1a',
+                color: '#f44336',
             },
             {
                 id: 2,
@@ -226,7 +204,7 @@ export default {
                 id: 3,
                 priority: 'High',
                 priority_id: '3',
-                color: 'yellow',
+                color: 'yellow ',
             },
             {
                 id: 4,
@@ -238,7 +216,7 @@ export default {
                 id: 5,
                 priority: 'Low',
                 priority_id: '5',
-                color: '#E0F0FF',
+                color: '#47b54c',
             }
         ]
         var priority = null
@@ -250,22 +228,45 @@ export default {
         return priority
     },
     countTasks(tasks){
+        var result = {}
         var disabled = [
-            'ready for testing',
             'closed',
             'hold',
-            'development plan',
             'cancelled',
         ]
-        var priorities = Object.keys(tasks)
-        // console.log(priorities)
+        var keys = Object.keys(tasks)
         var count = 0;
-        priorities.map((option) => {
+        var highest = 0;
+        var high = 0;
+        var medium = 0;
+        var low = 0;
+        keys.map((option) => {
             if(!disabled.includes(option.toLowerCase())){
                 count += tasks[option].length
+                tasks[option].map(task => {
+                    if(task.fields && task.fields.priority && task.fields.priority.id){
+                        if(task.fields.priority.id == '1'){
+                            highest = highest + 1
+                        }
+                        if(task.fields.priority.id == '2'){
+                            high = high + 1
+                        }
+                        if(task.fields.priority.id == '3'){
+                            medium = medium + 1
+                        }
+                        if(task.fields.priority.id == '4'){
+                            low = low + 1
+                        }
+                    }
+                })
             }
         })
-        return count
+        result.total = count
+        result.highest = highest
+        result.high = high
+        result.medium = medium
+        result.low = low
+        return result
     },
     howLongAgo(date){
         var newDate  =  new Date(date)
@@ -328,26 +329,19 @@ export default {
         return ''
     },
     searchFilter(tasks, query, groupBy){
-        console.log("SEARCH FILTER GROUP BY ", groupBy)
         var result = []
-        // console.log(tasks)
         if(query !== ''){
             if(typeof(tasks) == 'object' && !tasks.length){
                 result = {}
                 var empList = Object.keys(tasks)
                 empList.map(empl => {
-                    // console.log(empl)
                     if(tasks[empl]){
                         var statuses = Object.keys(tasks[empl])
-                        // console.log('got statuses list', statuses)
                         if(statuses && statuses.length >0){
                             statuses.map(stat => {
-                                // console.log('status ', stat)
                                 if(tasks[empl][stat] && tasks[empl][stat].length >0){
                                     var tasksArr = tasks[empl][stat]
-                                    // console.log('got tasks arr', tasksArr)
                                     tasksArr.map(task => {
-                                        // console.log('task ', task.subject)
                                         var key = undefined
                                         if(groupBy == 'assignee' && task.fields.assignee){
                                             key = task.fields.assignee.displayName.toLowerCase()
@@ -357,6 +351,9 @@ export default {
                                         }
                                         if(groupBy == 'team' && task.fields.customfield_10102){
                                             key = task.fields.customfield_10102.value.toLowerCase()
+                                        }
+                                        if(groupBy == 'pm' && task.fields.customfield_10106){
+                                            key = task.fields.customfield_10106.displayName.toLowerCase()
                                         }
                                         if(task.fields.summary.toLowerCase().includes(query.toLowerCase()) 
                                         || task.key.toLowerCase().includes(query.toLowerCase())
@@ -378,14 +375,25 @@ export default {
             }
             else {
                 tasks.map(item => {
-                    if(item.fields.summary.toLowerCase().includes(query.toLowerCase()) 
-                    || item.key.toLowerCase().includes(query.toLowerCase())
-                    || (item.fields.assignee && item.fields.assignee.displayName.toLowerCase().includes(query.toLowerCase()))){
+                    if(
+                        item.fields.summary.toLowerCase().includes(query.toLowerCase()) 
+                        || item.key.toLowerCase().includes(query.toLowerCase())
+                        ||  (
+                                item.fields.assignee 
+                                && groupBy !=='pm'
+                                && item.fields.assignee.displayName.toLowerCase().includes(query.toLowerCase())
+                            )
+                        ||  (
+                                item.fields.customfield_10106 
+                                && groupBy =='pm'
+                                && item.fields.customfield_10106.displayName.toLowerCase().includes(query.toLowerCase())
+                            )
+                        ){
+
                         result.push(item)
                     }
                 })
             }
-
         }
         if(result && Object.keys(result) && Object.keys(result).length >1){
             return result
